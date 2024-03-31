8361fcd4124ab6541283108692ddf2bc8562c3cb
talhacheema675 <125282171+talhacheema675@users.noreply.github.com>
2024-03-31 20:54:02 +0500

    Update header.tsx

--- src/compenent/header.tsx
+++ src/compenent/header.tsx
@@ -1,7 +1,11 @@
+"use client"
 import Image from "next/image"
+import { useState } from "react"
 import Link from "next/link"
+import { MenuIcon } from "lucide-react"
 const Header=()=>
 {
+    const [showval,setval]=useState(2)
     return(
         <div className="flex flex-row justify-between p-4 bg-black text-white">
            <Link href={"#"}> <div className="flex flex-row gap-1">
@@ -9,13 +13,14 @@ const Header=()=>
                 <h1 className="text-2xl">Talha cheema</h1>
             </div>
             </Link>
-            <div className="flex flex-row gap-8 text-xl">
-                    <Link href={"#1"}><h1>About</h1></Link>
+            <div className={`flex md:flex-row flex-col gap-8 text-xl ${showval%2==0?"hidden md:flex  ":""}  `}>
+                    <Link href={"#1"}><h1 className="md:pt-0 pt-20">About</h1></Link>
                     <Link href={"#2"}><h1>Portfolio</h1></Link>
                     <Link href={"#4"}><h1>Skills</h1></Link> 
                     <Link href={"#3"}><h1>contact</h1></Link>
                     <Link href={"#"}><h1>Login</h1></Link>
             </div>
+            <div className="md:hidden"> <button onClick={()=>setval(showval+1)}><MenuIcon/></button></div>
         </div>
     )
 }
