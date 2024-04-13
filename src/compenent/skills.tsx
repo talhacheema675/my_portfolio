c871aa16fdc1558e4a562fff93c6535d71305bb4
talhacheema675 <125282171+talhacheema675@users.noreply.github.com>
2024-04-13 16:49:05 +0500

    Update skills.tsx

--- src/compenent/skills.tsx
+++ src/compenent/skills.tsx
@@ -31,73 +31,73 @@ const Skill=()=>{
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/java.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/java.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Javascript</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/next2.jpg"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/next2.jpg"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Next JS</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/sql.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/sql.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">My SQL</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/sanity.webp"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/sanity.webp"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Sanity</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/drizzle.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/drizzle.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">DRIZZLE</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/vercel.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/vercel.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Vercel</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/nextauth.jpeg"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/nextauth.jpeg"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Next Auth</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/poet.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/poet.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Poetry</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/postman.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/postman.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Postman</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/logo1.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/logo1.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">React</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/shadcn.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/shadcn.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">Shadcn UI</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
                 </div>
                 <div className="flex flex-col text-center items-center gap-2 m-4">
-                    <Image src={"/git.png"} alt="html" width={"200"} height={"200"} className="w-44 h-44 rounded-full"/>
+                    <Image src={"/git.png"} alt="html" width={"200"} height={"200"} className="md:w-44 w-80 md:h-44 h-36 rounded-full"/>
                     <h1 className="text-2xl text-bold">GITHUB</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident doloremque molestias</p>
 
